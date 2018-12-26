9977f588e2af09eed12dfe87e116e73d1aa1353b
xiaolizi1972 <2860265796@qq.com>
2018-12-26 15:27:01 +0800

    新增数据迁移

---
+++ database/seeds/Users.php
@@ -0,0 +1,29 @@
+<?php
+
+use think\migration\Seeder;
+
+class Users extends Seeder
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+
+		 $rows = [
+            [
+              'username'  => 'xiaolizi'
+            ],
+            [
+              'username'  => 'alibaba'
+            ]
+        ];
+
+        $this->table('users')->insert($rows)->save();
+    }
+}
